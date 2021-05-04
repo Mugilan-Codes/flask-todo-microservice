8297671d948292b7873859b71213c800fdce48ac
Mugilan <mugilancodes@gmail.com>
2021-05-04 22:50:25 +0530

    feat: todo service

--- services/todo.py
+++ services/todo.py
@@ -18,5 +18,26 @@ def hello():
     return "Todo service is up"
 
 
+@app.route("/lists", methods=["GET"])
+def show_lists():
+    """Displays all the lists"""
+
+    tlists = []
+    for username in todo_list:
+        for lname in todo_list[username]:
+            tlists.append(lname)
+    return jsonify(lists=tlists)
+
+
+@app.route("/lists/<username>", methods=["GET"])
+def user_list(username):
+    """Returns a user oriented list"""
+
+    if username not in todo_list:
+        return "No list found"
+
+    return jsonify(todo_list[username])
+
+
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
